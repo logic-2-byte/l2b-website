5ee824777eb3d942540b18cf9fcb98109271b82a
Deepak-Appxcess <deepak@appxcess.com>
2026-04-27 03:58:36 +0530

    v2

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -22,7 +22,7 @@ export default function ContactPage() {
     visible: {
       y: 0,
       opacity: 1,
-      transition: { duration: 0.8, ease: [0.16, 1, 0.3, 1] }
+      transition: { duration: 0.8, ease: [0.16, 1, 0.3, 1] as any }
     }
   };
 
